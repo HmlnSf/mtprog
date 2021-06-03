99d7f0e7f051fc2bf9fe4a79ce377eead3a2273f
HmlnSf <60872931+HmlnSf@users.noreply.github.com>
2021-06-04 01:32:00 +0200

    Update main.cpp

--- sem_sysv/main.cpp
+++ sem_sysv/main.cpp
@@ -1,28 +1,17 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
+#include <fcntl.h>           
+//
+
+#include <sys/stat.h>        
+
+//
+#include <semaphore.h>
+//
 #include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
 
-union semun {
-    int              val;    /* Value for SETVAL */
-    struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-    unsigned short  *array;  /* Array for GETALL, SETALL */
-    struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                (Linux specific) */
-};
 
-int main()
-{
-	key_t semkey = ftok("/tmp/sem.temp", 1); 
-	int semd = semget( semkey, 16, IPC_CREAT | 0666 );
-	unsigned short semaphores[16];
-	for( int i = 0; i < 16; i++ ) {
-		semaphores[i] = i;
-	}
-	semun sun;
-	sun.array = semaphores;
-	semctl( semd, 0, SETALL, sun );
-	return 0;
+int main() {
+    sem_unlink("/test.sem");
+    sem_t *semid = sem_open("/test.sem", 0666 | O_CREAT, 0, 66);
+    pause();
+    return 0;
 }
